01e4d9904bc311d3d9066cf3630aa8a2b900b10b
Getama <31482656+Denergik@users.noreply.github.com>
2018-06-03 21:51:05 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client();
 
-bot.login(process.env.TOKEN);
+bot.login(NDUyODY5MzEyMjQwMjg3Nzgy.DfWnVA.eXDm4OckCeV8OtSM2mAR2Ula8D0);
 bot.on('message', message =>  {
 	if (message.content === '!ping') {
 		message.reply('pong !')
