b0ed18ca701fe0a648b9eb9e1c4b0ed507d49809
Getama <31482656+Denergik@users.noreply.github.com>
2018-06-04 00:57:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const bot = new Discord.Client();
 
 bot.on('ready', () => {
-    console.log('I am ready!');
+    console.log('Je suis prêt !');
 });
 
 bot.on('message', message =>  {
