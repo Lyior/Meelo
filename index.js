61fa3569630bc31a2273b0bd6e6f2981ecdbab17
Getama <31482656+Denergik@users.noreply.github.com>
2018-06-04 11:36:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ bot.on('message', message => {
                     color: 3447003,
                     author: {
 						name: message.guild.name,
-						icon_url: member.avatarURL
+						icon_url: ClientUser.avatarURL
 					},
 					title: 'Titre',
 					url: 'https://google.com',
@@ -90,7 +90,7 @@ bot.on('message', message => {
 					}],
 					timestamp: new Date(),
 					footer: {
-						icon_url: member.avatarURL,
+						icon_url: ClientUser.avatarURL,
 						text: "Udemy"
 					}
 				}
